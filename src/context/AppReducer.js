cc65b6dec6f1b1d9833276b89ebed08fa72b5a87
alifhs <alif.hs@outlook.com>
2021-08-22 13:05:43 +0600

    added add transaction

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
     // console.log(state, action)
+    // const newTransaction = {id : Math.floor(Math.random() * 10000000), amount : parseInt(action.payload.amount), text: action.payload.text};
+
     switch(action.type) {
        
         case 'DELETE_TRANSACTION':
@@ -10,6 +12,13 @@ export default (state, action) => {
             return {
                
                 transactions: state.transactions.filter(transaction=> transaction.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            console.log({
+                transactions : [...state.transactions, action.payload]
+            });
+            return {
+                transactions : [...state.transactions, action.payload]
             }
         default : 
             return state;
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -19,12 +19,21 @@ export const GlobalProvider = ({children}) => {
         const deleteTransaction = (id)=> {
             // console.log(id)
             dispatch({
-                type: 'DELETE_TRANSACTION',  // value passed to dispatch is action 
+                type: 'DELETE_TRANSACTION',  // arguments passed to dispatch is action 
                 payload: id
             })
         } 
+
+        const addTransaction = (newTransaction)=> {
+
+            console.log(newTransaction);
+            dispatch({
+                type: 'ADD_TRANSACTION',
+                payload: newTransaction
+            })
+        }
         return (
-            <GlobalContext.Provider value={{ transactions : state.transactions, deleteTransaction }}>
+            <GlobalContext.Provider value={{ transactions : state.transactions, deleteTransaction , addTransaction}}>
                 {children}
             </GlobalContext.Provider>
         )
